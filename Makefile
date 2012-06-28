1233bb344943789a9c49189ef50f350d5357fea9
Alexander Shabalin <shabalyn.a@gmail.com>
2012-06-28 18:27:23 +0400

    Removed dependency on ffmpeg for testing.

--- Makefile
+++ Makefile
@@ -5,16 +5,12 @@ CXXFLAGS = $(CFLAGS) -D__STDC_CONSTANT_MACROS
 INCPATH =
 LINK = g++ 
 LFLAGS = 
-LIBS = -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_video -lopencv_calib3d\
-		 -lavcodec -lavformat -lswscale -lavdevice -lavutil
+LIBS = -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_video -lopencv_calib3d
 
 all: stabilize
 
 stabilize: main.o stabilization.o
 	$(LINK) $(LFLAGS) -o stabilize main.o stabilization.o $(LIBS)
-
-#stabilize: main.o
-#	$(LINK) $(LFLAGS) -o stabilize main.o $(LIBS)
 
 main.o:
 	$(CXX) -c $(CXXFLAGS) $(INCPATH) main.cpp
--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "stabilization.h"
 #include <opencv2/opencv.hpp>
-
-extern "C" {
-   #include <libavcodec/avcodec.h>
-   #include <libavdevice/avdevice.h>
-   #include <libavformat/avformat.h>
-   #include <libswscale/swscale.h>
-}
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -22,78 +16,26 @@ void draw(char* windowName, int width, int height, void* img1, void* img2) {
    imshow(windowName,resSmall);
 }
 
-int initFFmpeg( char* fmt, char* name
-               , AVCodecContext **codec, AVFormatContext **format, SwsContext **scaler) {
-   avcodec_register_all();
-   avdevice_register_all();
-   av_register_all();
-   avformat_network_init();
-   *format = avformat_alloc_context();
-   if(avformat_open_input(format,name,av_find_input_format(fmt),0) < 0) {
-      cout << "Can't open input" << endl;
-      return -1;
-   }
-   if(avformat_find_stream_info(*format,0) < 0) {
-      cout << "Can't find stream info" << endl;
-      return -1;
-   }
-   *codec = (*format)->streams[0]->codec;
-   if(!codec) { cout << "Can't get codec context" << endl; return -1; }
-   AVCodec* decoder = avcodec_find_decoder((*codec)->codec_id);
-   if(!decoder) { cout << "Can't find decoder" << endl; return -1; }
-   if(avcodec_open2(*codec,decoder,0) < 0) {
-      cout << "Can't associate codec on input" << endl;
-      return -1;
-   }
-   cout << (*codec)->width << endl << (*codec)->height << endl;
-   *scaler = sws_getCachedContext( 0, (*codec)->width, (*codec)->height, (*codec)->pix_fmt
-                                , (*codec)->width, (*codec)->height, PIX_FMT_BGR24
-                                , SWS_BICUBIC, 0, 0, 0);
-   return 0;
-}
-
-Frame* readFrame(AVCodecContext* codec, AVFormatContext* format, SwsContext* scaler) {
-   AVPacket pkt;
-   AVFrame* frame;
-   int got_picture = 0;
-   while(!got_picture) {
-      if(av_read_frame(format,&pkt) < 0) cout << "Can't read frame" << endl;
-      frame = avcodec_alloc_frame();
-      if(avcodec_decode_video2(codec,frame,&got_picture,&pkt) < 0) {
-         cout << "Can't decode pkt" << endl;
-         continue;
-      }
-      if(!got_picture) continue;
-      AVFrame* newFrame = avcodec_alloc_frame();
-      avpicture_alloc((AVPicture*)newFrame,PIX_FMT_BGR24,codec->width,codec->height);
-      sws_scale(scaler,frame->data,frame->linesize,0,frame->height
-               ,newFrame->data,newFrame->linesize);
-      av_free(frame);
-      frame = newFrame;
-      av_free_packet(&pkt);
-   }
-   cout << frame->data[0];
-   Frame* packed_frame = new Frame(codec->width,codec->height,frame->data[0]);
-   avpicture_free((AVPicture*) frame);
-   av_free(frame);
-   return packed_frame;
-}
-
 int main(int argc, char *argv[]) {
-   AVCodecContext *codec;
-   AVFormatContext *format;
-   SwsContext* scaler;
-   if(initFFmpeg("avi","videos/vid1.avi",&codec,&format,&scaler) < 0) return -1;
-   Frame *lastFrame = readFrame(codec,format,scaler);
+   uint8_t* data;
+   int width = 640;
+   int height = 480;
+   int length = 3*width*height;
+
+   data = new uint8_t[length];
+   fread(data, 1, length, stdin);
+   Frame *lastFrame = new Frame(width, height, data);
+   delete [] data;
+
    namedWindow("stab",1);
    for(;;) {
-      //cout << "Reading new frame" << endl;
-      Frame *frame = readFrame(codec,format,scaler);
-      //cout << "Read new frame" << endl;
+      data = new uint8_t[length];
+      fread(data, 1, length, stdin);
+      Frame *frame = new Frame(width, height, data);
+      delete [] data;
       stabilize(lastFrame, frame);
-      //cout << "Stabilized frame" << endl;
       delete lastFrame;
-      draw( "stab", codec->width, codec->height
+      draw( "stab", width, height
           , frame->getOriginalImage(), frame->getStabilizedImage());
       lastFrame = frame;
       if(waitKey(30) >= 0) break;
